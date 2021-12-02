d2fc63d8225c7ecf7cc4d8cce659fa49bf12dee5
Aleskert <5354982+Aleskert@users.noreply.github.com>
2021-12-02 10:10:31 +0200

    v2.0
    Added:
    - subscribe
    - separate last pic for all users
    - scheduler

--- bot.py
+++ bot.py
@@ -23,6 +23,8 @@ import requests
 import logging
 from fake_useragent import UserAgent
 from telebot import types
+import threading
+import schedule
 
 # from PIL import Image
 
@@ -47,7 +49,8 @@ class SQLighter:
 
     def checkboobs(self, utc):
         with self.connection:
-            if self.cursor.execute('SELECT count(*) FROM goddesses WHERE utc = ?', (utc,)).fetchone()[0] == 1:
+            isboobs = self.cursor.execute('SELECT count(*) FROM goddesses WHERE utc = ?', (utc,)).fetchone()[0]
+            if isboobs > 0 :
                 return True
 
     def check_user(self, id):
@@ -63,6 +66,18 @@ class SQLighter:
             except sqlite3.DatabaseError as error:
                 print("Error add_user:", error)
 
+    def all_reg_users(self):
+        with self.connection:
+            return self.connection.execute('SELECT ID FROM users').fetchall()
+
+    def user_last_pic(self, userid):
+        with self.connection:
+            return self.cursor.execute('SELECT lastpic FROM users WHERE id = ?', (userid,)).fetchone()[0]
+
+    def update_last_pic(self, utc, userid):
+        with self.connection:
+            self.cursor.execute('UPDATE users SET lastpic = ? WHERE ID = ?',(utc,userid))
+
 # =============================SQL END
 
 def read_js(req):
@@ -74,12 +89,18 @@ def read_js(req):
     return data
 
 
-def check_and_add(name, url, utc):
+def check_and_add(name, url, utc, userid):
     db_worker = SQLighter(config.database_name)
     if db_worker.checkboobs(utc):
-        print("   ", end=" | ")
-        print(name)
-        posted = 1
+        if db_worker.user_last_pic(userid) < utc:
+            print(" + ", end=" | ")
+            print(name)
+            db_worker.update_last_pic(utc, userid)
+            posted = 0
+        else:
+            print("   ", end=" | ")
+            print(name)
+            posted = 1
         db_worker.close()
     else:
         db_worker.addboobs(name, url, utc)
@@ -110,9 +131,9 @@ def get_picture(url, message):
                 name = del_amp(k['data']['title'])
                 url = del_amp(k['data']['preview']['reddit_video_preview']['fallback_url'])
                 time_create = k['data']['created_utc']
-                if check_and_add(name, url, time_create) == 0:
+                if check_and_add(name, url, time_create, message) == 0:
                     try:
-                        bot.send_video(message.chat.id, url)
+                        bot.send_video(message, url)
                     except Exception:
                         print("Error parsing video")
         except KeyError:
@@ -131,8 +152,8 @@ def get_picture(url, message):
                                 url = del_amp(k['data']['preview']['images'][0]['resolutions'][1]['url'])
                             except Exception:
                                 print("Ніхуя не вдалося, певно відсутні дані")
-                    if check_and_add(name, url, time_create) == 0:
-                        bot.send_photo(message.chat.id, url, caption=name)
+                    if check_and_add(name, url, time_create, message) == 0:
+                        bot.send_photo(message, url, caption=name)
                 else:
                     print('======= ', name)
             except KeyError:
@@ -153,15 +174,26 @@ def get_picture(url, message):
                     images += [types.InputMediaPhoto(url, name)]
                     sql_url += str(url) + ','
                 time_create = k['data']['created_utc']
-                if check_and_add(name, url, time_create) == 0:
-                    bot.send_media_group(message.chat.id, images)
+                if check_and_add(name, url, time_create, message) == 0:
+                    try:
+                        bot.send_media_group(message, images)
+                    except telebot.apihelper.ApiException as e:
+                        if e.result.status_code == 403:
+                            print('Користувач забанив бота')
+                        elif e.result.status_code == 400:
+                            print('Не вийшло загрузить медіа')
         except KeyError:
             pass
 
+def all_users():
+    db_worker = SQLighter(config.database_name)
+    v_all_users = db_worker.all_reg_users()
+    db_worker.close()
+    return v_all_users
 
 @bot.message_handler(content_types=['document', 'audio'])
 def handle_docs_audio(message):
-    bot.send_message(message.chat.id, "Аудіо бот не сприймає")
+    bot.send_message(message.chat.id, "Файли бот не сприймає")
 
 
 @bot.message_handler(commands=['help'])
@@ -175,56 +207,39 @@ def open_help(message):
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, "Бот запущено")
 
-@bot.message_handler(func=lambda message: True, commands=['goddesses'])
-def goddesses(message):
-    print('----------Goddesses----------', end='| ')
-    print(time.localtime().tm_hour, ":", time.localtime().tm_min, end=' | ')
-    print(message.from_user.id, "|", message.from_user.first_name, end=' | ')
-    print(message.from_user.last_name, "|" , message.from_user.username)
+    db_worker = SQLighter(config.database_name)
+    if db_worker.check_user(message.from_user.id):
+        try:
+            bot.send_message(message.chat.id, "Бот запущено")
+            # get_picture(url, message)
+        except telebot.apihelper.ApiException as e:
+            if e.result.status_code == 403:
+                print('Користувач забанив бота')
+            elif e.result.status_code == 400:
+                print('Не вийшло загрузить медіа')
+    else:
+        keyboard = types.ReplyKeyboardMarkup(row_width=1, one_time_keyboard=True, resize_keyboard=True)
+        button_phone = types.KeyboardButton(request_contact=True, text="Надіслати свої дані")
+        keyboard.add(button_phone)
+        bot.send_message(message.chat.id, 'Ти не зареєстрований. Зареєструйся!', reply_markup=keyboard)
+    db_worker.close()
+
+# @bot.message_handler(commands=['goddesses'])
+def goddesses():
     url = 'https://www.reddit.com/r/goddesses/new/.json'
+    v_all_users = all_users()
+    for v in v_all_users:
+        print(v[0],"===========================GODDESSES")
+        get_picture(url, v[0])
 
-    db_worker = SQLighter(config.database_name)
-    if db_worker.check_user(message.from_user.id):
-        try:
-            get_picture(url, message)
-        except telebot.apihelper.ApiException as e:
-            if e.result.status_code == 403:
-                print('Користувач забанив бота')
-            elif e.result.status_code == 400:
-                print('Не вийшло загрузить медіа')
-    else:
-        keyboard = types.ReplyKeyboardMarkup(row_width=1, one_time_keyboard=True, resize_keyboard=True)
-        button_phone = types.KeyboardButton(request_contact=True, text="Надіслати свої дані")
-        keyboard.add(button_phone)
-        bot.send_message(message.chat.id, 'Ти не зареєстрований. Зареєструйся!', reply_markup=keyboard)
-    db_worker.close()
-
-
-@bot.message_handler(commands=['nsfw'])
-def nsfw(message):
-    print('----------NSFW----------', end='| ')
-    print(time.localtime().tm_hour, ":", time.localtime().tm_min, end=' | ')
-    print(message.from_user.id, "|", message.from_user.first_name, end=' | ')
-    print(message.from_user.last_name, "|" , message.from_user.username)
+def nsfw():
     url = 'https://www.reddit.com/r/nsfw/new/.json'
+    v_all_users = all_users()
+    for v in v_all_users:
+        print(v[0], "===========================NSFW")
+        get_picture(url, v[0])
 
-    db_worker = SQLighter(config.database_name)
-    if db_worker.check_user(message.from_user.id):
-        try:
-            get_picture(url, message)
-        except telebot.apihelper.ApiException as e:
-            if e.result.status_code == 403:
-                print('Користувач забанив бота')
-            elif e.result.status_code == 400:
-                print('Не вийшло загрузить медіа')
-    else:
-        keyboard = types.ReplyKeyboardMarkup(row_width=1, one_time_keyboard=True, resize_keyboard=True)
-        button_phone = types.KeyboardButton(request_contact=True, text="Надіслати свої дані")
-        keyboard.add(button_phone)
-        bot.send_message(message.chat.id, 'Ти не зареєстрований. Зареєструйся!', reply_markup=keyboard)
-    db_worker.close()
 
 @bot.message_handler(content_types=['contact'])
 def contact(message):
@@ -237,12 +252,22 @@ def contact(message):
                            message.from_user.is_bot)
         db_worker.close()
         keyboard = types.ReplyKeyboardRemove()
-        bot.send_message(message.chat.id, "Зарєстровано", reply_markup=keyboard)
+        bot.send_message(message.chat.id, "Зарєстровано! Зачекай, поки адмін додасть тебе до дозволених людей!", reply_markup=keyboard)
     else:
         bot.send_message(message.chat.id, 'Ділитися треба лише СВОЇМИ контактами')
 
-if __name__ == '__main__':
-    logging.getLogger('telebot').setLevel(logging.DEBUG)
-    logging.basicConfig(format='[%(asctime)s] %(filename)s:%(lineno)d %(levelname)s - %(message)s', level=logging.INFO,
-                        filename='bot_log.log', datefmt='%d.%m.%Y %H:%M:%S')
+def runBot():
     bot.polling(none_stop=True)
+
+def runScheluders():
+    schedule.every().hour.at(":26").do(goddesses)
+    schedule.every().hour.at(":42").do(nsfw)
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+if __name__ == '__main__':
+    t1 = threading.Thread(target=runBot)
+    t2 = threading.Thread(target=runScheluders)
+    t1.start()
+    t2.start()
